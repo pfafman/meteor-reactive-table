0961a9543895f4ff34fe33bb9937658bd6b304cc
Timothy Pfafman <tim@pfafman.com>
2015-07-07 14:36:54 -0700

    no fixed footer

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'pfafman:reactive-table',
   summary: "Paging Table for Meteor",
-  version: "0.1.1",
+  version: "0.1.2",
   git: "https://github.com/pfafman/meteor-reactive-table.git",
 });
 
