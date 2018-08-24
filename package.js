0544a3336983d184a230b944b974f29cc7089139
Timothy Pfafman <tim@pfafman.com>
2018-08-24 12:07:39 -0600

    fixing issue with change in underscore/coffeescript

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'pfafman:reactive-table',
   summary: "Paging Table for Meteor",
-  version: "0.4.11",
+  version: "0.4.12",
   git: "https://github.com/pfafman/meteor-reactive-table.git",
 });
 
