6d2bda1b036b97328a8c6b5ba23ad2f4d5eb457a
Timothy Pfafman <tim@pfafman.com>
2020-05-26 06:30:11 -0600

    more options

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'pfafman:reactive-table',
   summary: "Paging Table for Meteor",
-  version: "0.4.16",
+  version: "0.4.17",
   git: "https://github.com/pfafman/meteor-reactive-table.git",
 });
 
